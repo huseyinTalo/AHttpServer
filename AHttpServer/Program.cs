fbf5b1d3f20242c30240024e30bd72fdb8a2c7b4
huseyinTalo <huseyintaloo@gmail.com>
2025-06-02 19:08:16 +0300

    get, post, put, delete eklendi ayri caseler olarak

--- AHttpServer/Program.cs
+++ AHttpServer/Program.cs
@@ -3,36 +3,42 @@ using System.Net.Sockets;
 using System.Text;
 
 
-	int port =	6851;
+int port = 6851;
 
-	IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+IPAddress localAddr = IPAddress.Parse("127.0.0.1");
 
-	TcpListener tcpListener = new TcpListener(localAddr, port);
+TcpListener tcpListener = new TcpListener(localAddr, port);
 try
 {
 
-	tcpListener.Start();
+    tcpListener.Start();
 
 
-	while (true)
-	{
-		Console.WriteLine($"Listening on {port}");
+    while (true)
+    {
+        Console.WriteLine($"Listening on {port}");
 
-		using TcpClient tcpClient = tcpListener.AcceptTcpClient();
+        using TcpClient tcpClient = tcpListener.AcceptTcpClient();
 
-		byte[] data = new byte[512];
+        byte[] data = new byte[512];
 
-		int offset = 0;
+        int offset = 0;
 
-		int count = 512;
+        int count = 512;
 
-		tcpClient.GetStream().Read(data, offset, count);
+        tcpClient.GetStream().Read(data, offset, count);
 
-		if (data[0] == (int)'G' && data[1] == (int)'E' && data[2] == (int)'T')
-		{ 
+        string message = string.Empty;
 
-		Console.WriteLine(data.ToString());
-		 
+        foreach (var item in data)
+        {
+            message += (char)item;
+        }
+
+        string[] messageArray = message.Split(" ");
+
+        string firstWord = messageArray[0];
+
         string httpResponse =
            "HTTP/1.1 200 OK\r\n" +
            "Content-Type: text/plain\r\n" +
@@ -42,18 +48,32 @@ try
 
         byte[] responseBytes = Encoding.UTF8.GetBytes(httpResponse);
 
-        tcpClient.GetStream().Write(responseBytes, 0, responseBytes.Length);
 
-		}
 
-			tcpClient.Close();
-	}
+        switch (firstWord)
+        {
+            case "GET":
+                tcpClient.GetStream().Write(responseBytes, 0, responseBytes.Length);
+                break;
+            case "POST":
+                tcpClient.GetStream().Write(responseBytes, 0, responseBytes.Length);
+                break;
+            case "PUT":
+                tcpClient.GetStream().Write(responseBytes, 0, responseBytes.Length);
+                break;
+            case "DELETE":
+                tcpClient.GetStream().Write(responseBytes, 0, responseBytes.Length);
+                break;
+        }
+
+        tcpClient.Close();
+    }
 }
 catch (Exception ex)
 {
-	throw;
+    throw;
 }
 finally
 {
-	tcpListener.Stop();
+    tcpListener.Stop();
 }
